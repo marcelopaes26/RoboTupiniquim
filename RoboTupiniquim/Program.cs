4f1b9dd0ac6e51c9d67f7675d6b36a6978cc820e
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-04-17 21:46:10 -0300

    Corrigido o bug na versão sequencial

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -2,14 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        string posicaoInicial = "5 5 ";
+        string posicaoInicial = "3 3 L";
         string comando = "MMDMMDMDDM";
 
         string[] coordenadasIniciais = posicaoInicial.Split(' ');
 
         int posicaoX = Convert.ToInt32(coordenadasIniciais[0]);
-        int posicaoY = Convert.ToInt32(coordenadasIniciais[1]); 
-        char direcao = Convert.ToChar(coordenadasIniciais[2]); 
+        int posicaoY = Convert.ToInt32(coordenadasIniciais[1]);
+        char direcao = Convert.ToChar(coordenadasIniciais[2]);
 
         char[] instrucoes = comando.ToCharArray();
 
@@ -35,27 +35,27 @@
                 {
                     direcao = 'N';
                 }
-                else if (direcao == 'M')
-                {
-                    if (direcao == 'N')
-                    {
-                        posicaoY++;
-                    }
-                    else if (direcao == 'S')
-                    {
-                        posicaoY--;
-                    }
-                    else if (direcao == 'O')
-                    {
-                        posicaoX--;
-                    }
-                    else if (direcao == 'L')
-                    {
-                        posicaoX++;
-                    }
-                }
             }
             else if (instrucaoAtual == 'D')
+            {
+                if (direcao == 'N')
+                {
+                    direcao = 'L';
+                }
+                else if (direcao == 'L')
+                {
+                    direcao = 'S';
+                }
+                else if (direcao == 'S')
+                {
+                    direcao = 'O';
+                }
+                else if (direcao == 'O')
+                {
+                    direcao = 'N';
+                }
+            }
+            else if (instrucaoAtual == 'M')
             {
                 if (direcao == 'N')
                 {
